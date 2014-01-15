0b909b89922db4357cdccba236ec6518e8e6c7d7
Ciarán Archer <carcher@zendesk.com>
2014-01-15 20:18:16 +0000

    Add tests for cat-many

--- test/twilio_recordings/core_test.clj
+++ test/twilio_recordings/core_test.clj
@@ -1,8 +1,18 @@
 (ns twilio-recordings.core-test
+  [:require [clojure.java [io :as io]]]
   (:use clojure.test
         twilio-recordings.core))
 
-(deftest test-urls
+(def temp-files '("/tmp/piece1.dat" "/tmp/piece2.dat" "/tmp/piece3.dat"))
+
+(defn file-setup [f]
+  (dorun (map #(io/writer %) temp-files))
+  (f)
+  (dorun (map #(io/delete-file %) temp-files)))
+
+(use-fixtures :each file-setup)
+
+(deftest test-urls-files
   (def recordings-sids '("RE123" "RE456" "RE789"))
   (def acc-sid "AC112233")
 
@@ -12,3 +22,9 @@
                     {:url "http://api.twilio.com/2010-04-01/Accounts/AC112233/Recordings/RE789" :file "/tmp/RE789.mp3"}))
     (is (= (twilio-recordings.core/urls-files acc-sid recordings-sids) expected))))
 
+
+(deftest test-cat-many
+  (testing "concatenating many files"
+    (def target-file "/tmp/test.dat")
+    (twilio-recordings.core/cat-many target-file temp-files)
+    (is (= (.exists (io/as-file target-file)) true))))
