ddeaf7625d2e3595cd1f9dbfdbbe6dc290a0f2dc
Ciarán Archer <carcher@zendesk.com>
2014-01-16 19:04:01 +0000

    Clean up test namespaces.

--- test/twilio_recordings/core_test.clj
+++ test/twilio_recordings/core_test.clj
@@ -1,7 +1,7 @@
 (ns twilio-recordings.core-test
   [:require [clojure.java [io :as io]]]
-  (:use clojure.test
-        twilio-recordings.core))
+  [:use [twilio-recordings [core :as twl-recs]]]
+  [:use clojure.test])
 
 (def temp-files '("/tmp/piece1.dat" "/tmp/piece2.dat" "/tmp/piece3.dat"))
 
@@ -20,18 +20,18 @@
                     {:url "http://api.twilio.com/2010-04-01/Accounts/AC112233/Recordings/RE789" :file "/tmp/RE789.mp3"})]
 
   (testing "creating file paths and urls from recordings sids"
-    (is (= (twilio-recordings.core/urls-files acc-sid recordings-sids) expected)))))))
+    (is (= (twl-recs/urls-files acc-sid recordings-sids) expected)))))))
 
 (deftest test-cat-many
   (testing "concatenating many files"
     (let [target-file "/tmp/test.dat"]
-    (twilio-recordings.core/cat-many target-file temp-files)
+    (twl-recs/cat-many target-file temp-files)
     (is (= (.exists (io/as-file target-file)) true)))))
 
 (deftest test-fetch-url ;This will hit the internets. Alternative solutions?
   (testing "fetching urls and saving to disk"
     (let [target-file "/tmp/doge.jpg"]
-    (twilio-recordings.core/fetch-url "http://i.huffpost.com/gen/1451579/thumbs/o-DOGE-570.jpg?6" target-file)
+    (twl-recs/fetch-url "http://i.huffpost.com/gen/1451579/thumbs/o-DOGE-570.jpg?6" target-file)
     (is (= (.exists (io/as-file target-file)) true))
     (io/delete-file target-file))))
 
@@ -42,5 +42,5 @@
                            "REcd5c5bec7ff667d5c3f1502d04ccb79e"
                            "REf7b24375cdbbbb42f58828f2fdf7b5a9"
                            "RE23c45b6262e256a455b1ee296af53fbb")]
-    (twilio-recordings.core/fetch "ACeb4e7b38952d70a91bc4a4acea8dc9e0" recording-sids target-file)
+    (twl-recs/fetch "ACeb4e7b38952d70a91bc4a4acea8dc9e0" recording-sids target-file)
     (is (= (.exists (io/as-file target-file)) true))))))
